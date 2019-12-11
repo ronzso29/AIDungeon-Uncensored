5ea76736bfa6398b25c9e126634fe1793cb73043
ronzso29 <58788587+ronzso29@users.noreply.github.com>
2019-12-11 14:41:05 -0500

    Update story_manager.py

--- story/story_manager.py
+++ story/story_manager.py
@@ -128,7 +128,7 @@ class Story():
         f.close()
 
         FNULL = open(os.devnull, 'w')
-        p = Popen(['gsutil', 'cp', file_name, 'gs://aidungeonstories'], stdout=FNULL, stderr=subprocess.STDOUT)
+        #p = Popen(['gsutil', 'cp', file_name, 'gs://aidungeonstories'], stdout=FNULL, stderr=subprocess.STDOUT)
         return self.uuid
 
     def load_from_storage(self, story_id):
